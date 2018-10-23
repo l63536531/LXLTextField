4f83d9867f2c671211723d5ba021740f9e7af897
xiaolong <lxlzy321@163.com>
2018-10-23 21:45:53 +0800

    add

---
+++ LXLTextField/lxlViewController.h
@@ -0,0 +1,13 @@
+//
+//  lxlViewController.h
+//  LXLTextField
+//
+//  Created by Apple on 2018/10/23.
+//  Copyright © 2018年 XNB4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface lxlViewController : UIViewController
+
+@end
---
+++ LXLTextField/lxlViewController.m
@@ -0,0 +1,37 @@
+//
+//  lxlViewController.m
+//  LXLTextField
+//
+//  Created by Apple on 2018/10/23.
+//  Copyright © 2018年 XNB4. All rights reserved.
+//
+
+#import "lxlViewController.h"
+
+@interface lxlViewController ()
+
+@end
+
+@implementation lxlViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
